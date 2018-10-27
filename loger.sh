bdf9458c49431b61b5271721c8c5a95c01568116
DESKTOP-DELRI4U\kuros <mikisatoshi1992@gmail.com>
2018-10-27 16:25:58 +0900

    commit

--- loger.sh
+++ loger.sh
@@ -2,3 +2,13 @@
 ls
 cd pi_loger
 python loger.py
+cd
+files="/home/pi/repo_loger/storage/photo/*"
+for pathfile in $files
+do
+  echo $pathfile
+  name="${pathfile##*/}"
+  echo $name
+  ./dropbox_uploader.sh upload $pathfile /home/pi/projects/pi_loger/photo/$name
+  rm $pathfile
+done
--- scheduler.sh
+++ scheduler.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-ls
+# ls
 # cd pi_loger
 # git pull origin master
 
-rm -rf pi_loger
+ping -c 1 google.com
+echo $?
+if [ "$?" = "0" ] ; then
+  rm -rf pi_loger
+fi
 git clone https://github.com/mikisatoshi/pi_loger.git
+
+cd pi_loger
+chmod 777 upd.sh
+./upd.sh
